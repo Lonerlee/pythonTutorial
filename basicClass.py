fe5d05250945ed3d1e7f72229c2ad6d7e6c62805
Lonerlee <42679050+Lonerlee@users.noreply.github.com>
2022-12-26 18:41:40 +0100

    Update basicClass.py
    Adding diameter function to radiusCircle

--- basicClass.py
+++ basicClass.py
@@ -27,5 +27,9 @@ class radiusCircle:
   def area(self):
     return self.radius ** 2 * math.pi
 
+  def diameter(self):
+    return self.radius * 2
+
 circle = radiusCircle(25)
 print(circle.area())
+print(circle.diameter())
