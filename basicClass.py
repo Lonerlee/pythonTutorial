abe166a99ec1d4f46f036ee419c20abbe04c7b61
Lonerlee <42679050+Lonerlee@users.noreply.github.com>
2022-12-29 13:10:48 +0100

    Update basicClass.py
    Adding some comments

--- basicClass.py
+++ basicClass.py
@@ -5,13 +5,16 @@ current_time = datetime.datetime.now()
 clock_time = current_time.strftime("%H:%M:%S")
 
 class Clock:
+  #assigning variables
   def __init__(self, type, time):
     self.type = type
     self.time = time
 
+  #showing the current time
   def show_time(self):
     print(f"The time right now - {self.time}")
-    
+  
+  #showing the type of the clock
   def show_type(self):
     print(f"The type of the clock is - {self.type}.")
 
@@ -21,15 +24,19 @@ clock1.show_time()
 clock1.show_type()
 
 class operationsCircle:
+  #getting the variables
   def __init__(self, radius):
     self.radius = radius
 
+  #calculating an area
   def area(self):
     return self.radius ** 2 * math.pi
 
+  #calculating a diameter
   def diameter(self):
     return self.radius * 2
     
+  #calculating a circumference
   def circumference(self):
     return self.radius * 2 * math.pi
 
