fb649888d6ecb19f71cfbbc7ecc6c9aec95dad53
Lonerlee <42679050+Lonerlee@users.noreply.github.com>
2023-01-01 22:30:49 +0100

    Update webScrap.py
    Creating code that prints out a title of any website!

--- webScrap.py
+++ webScrap.py
@@ -1,7 +1,14 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("youtube.com", "r") as f:
-  doc = BeautifulSoup(f, "html.parser")
+urlDefault = "https://www.youtube.com/watch?v=EOWP5Y7eErE"
+urlDefault = input("Type in any URL and it will provide you with a page title (Make sure to add 'https://') - ")
 
-tag = doc.title
-print(tag)
+page = requests.get(urlDefault)
+zupka = BeautifulSoup(page.text, 'html.parser')
+
+pageTitle = []
+for title in zupka.find_all('title'):
+    pageTitle.append(title.text.strip())
+
+print(pageTitle)
