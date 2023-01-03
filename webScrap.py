f218caa9eabdad3814f56f02f03dfe3ecbcfc352
Lonerlee <sokalsebastian96@gmail.com>
2023-01-03 22:17:03 +0100

    Adding https:// instead of users having to write it down themselves.

--- webScrap.py
+++ webScrap.py
@@ -1,10 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-urlDefault = "https://www.youtube.com/watch?v=EOWP5Y7eErE"
-urlDefault = input("Type in any URL and it will provide you with a page title (Make sure to add 'https://') - ")
+https = "https://"
 
-page = requests.get(urlDefault)
+urlDefault = "https://www.youtube.com/watch?v=EOWP5Y7eErE"
+urlDefault = input("Type in any URL and it will provide you with a page title - https://")
+
+page = requests.get(https + urlDefault)
 zupka = BeautifulSoup(page.text, 'html.parser')
 
 pageTitle = []
@@ -12,6 +14,3 @@ for title in zupka.find_all('title'):
     pageTitle.append(title.text.strip())
 
 print(pageTitle)
-print(pageTitle)
-
-#Testing push
