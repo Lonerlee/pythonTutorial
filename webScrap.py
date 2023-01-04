b869f9990e1278ba1596da27d6942ee0a5ada318
Lonerlee <sokalsebastian96@gmail.com>
2023-01-04 22:09:21 +0100

    Adding a class that lets you decide if you want to use your own website or predefined

--- webScrap.py
+++ webScrap.py
@@ -11,11 +11,23 @@ class Decision:
     self.decide = decide
 
   def myfunc(self):
-    
+    decide = "Y"
+    i = 0
+    while i < 1:
+      decide = input("Type Y if you want to use your own page or N if you want to use default one [" + urlDefault + "]? - ")
+      if decide == "Y" or decide == "y":
+        return input("Type in any URL and it will provide you with a page title - https://")
+        i += 1
+      elif decide == "N" or decide == "n":
+        return "www.google.com"
+        i += 1
+      else :
+        print("Error. Try to type single letter Y or N.")
 
-decide = input("Do you want to use your own page or do you want to use default one [" + urlDefault + "]? Type Y or N - ")
 
-urlDefault = input("Type in any URL and it will provide you with a page title - https://")
+d1 = Decision("www.google.com")
+
+urlDefault = d1.myfunc()
 
 page = requests.get(https + urlDefault)
 zupka = BeautifulSoup(page.text, 'html.parser')
