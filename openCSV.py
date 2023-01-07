b2e0127dd3f06f8a29b0dbec7323407ee5ff3036
Lonerlee <sokalsebastian96@gmail.com>
2023-01-07 19:39:44 +0100

    Changing values and polishing the code

--- openCSV.py
+++ openCSV.py
@@ -3,5 +3,14 @@ import csv
 file = open('NewCSVFile.csv')
 szukacz = csv.reader(file)
 
+print("Shows all values of the CSV file:")
+
 for line in szukacz:
     print(line)
+
+print("Shows every single column name:")
+
+with open("NewCSVFile.csv") as csvfile:
+    reader = csv.DictReader(csvfile)
+    nameOfData = reader.fieldnames
+    print(nameOfData)
