fb7fd43c8b605f9dd7304e646b2191971d9e70e1
Lonerlee <sokalsebastian96@gmail.com>
2023-01-10 22:50:33 +0100

    Adding matlib

---
+++ matlib.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [10, 20, 30, 42]
+y = [20, 30, 40, 69]
+
+plt.plot(x, y)
+
+plt.title("Simple Plot")
+
+plt.ylabel("y-axis")
+plt.xlabel("x-axis")
+plt.show()
