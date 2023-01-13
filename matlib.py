23b32190023b61a10a6e950882ae7944de30a4bd
Lonerlee <sokalsebastian96@gmail.com>
2023-01-13 22:42:49 +0100

    Polishing matlib

--- matlib.py
+++ matlib.py
@@ -12,9 +12,8 @@ myGraph.title(input("Add title for the graph - "))
 
 myGraph.xlabel(input("Add name for X axis - "))
 myGraph.ylabel(input("Add name for Y axis - "))
-myGraph.savefig("images/graph.png")
 
 myGraph.style.use('fivethirtyeight')
-myGraph.savefig("images/graphNewStyle.png")
+myGraph.savefig("images/graph.png")
 
 print("Graph got saved at images/graph.png")
