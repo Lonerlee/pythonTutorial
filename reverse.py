c23280a261de95224a4427125ac61d41d49acd14
Lonerlee <sokalsebastian96@gmail.com>
2023-01-14 21:26:39 +0100

    Adding some operations on strings.

---
+++ reverse.py
@@ -0,0 +1,29 @@
+space = ' '
+
+name = input('Input the word or sentence to reverse it, write down verticaly and make the stairs. ')
+
+reverse = ''
+
+print('Reversed:')
+
+for char in name:
+    reverse = char + reverse
+    
+print(reverse)
+
+print('Vertical:')
+
+spellOut = ''
+
+for char in name:
+    spellOut = char + space
+    print(spellOut)
+
+print('Staris:')
+
+stairs = ''
+
+for char in name:
+    stairs = stairs + char
+    print(stairs)
+
