d2f3e7c0f662079c2ee14d5559658831fc3aad92
Lonerlee <sokalsebastian96@gmail.com>
2023-01-18 21:51:44 +0100

    Checking if removing HTML is going to work with different types of tags

--- requestsX.py
+++ requestsX.py
@@ -6,14 +6,14 @@ def cleanhtml(raw_html):
   cleantext = re.sub(CLEANR, '', raw_html)
   return cleantext
 
-x = requests.get("https://api.dane.gov.pl/1.4/datasets/1434,obligacje-skarbowe-1?lang=pl")
+x = requests.get("https://api.dane.gov.pl/1.4/resources/44385,liczba-studentow-z-obywatelstwem-ukrainskim-stan-na-09012023?lang=pl")
 works = x.json()
 
 print('Title:')
-print(works['data']['attributes']['category']['title'])
+print(works['data']['attributes']['title'])
 
 print('Description:')
-print(cleanhtml(works['data']['attributes']['notes']))
+print(cleanhtml(works['data']['attributes']['description']))
 
-print('License:')
-print(works['data']['attributes']['license_name'])
+print('Language:')
+print(works['meta']['params']['lang'])
