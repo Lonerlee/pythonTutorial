886230620fc3bf233ae52f55c8580b80b6ff0447
Lonerlee <sokalsebastian96@gmail.com>
2023-01-20 21:48:43 +0100

    Adding commands

--- requestsX.py
+++ requestsX.py
@@ -9,14 +9,19 @@ def cleanhtml(raw_html):
 x = requests.get("https://api.dane.gov.pl/1.4/resources/43547,wykaz-publikacji-naukowych-i-specjalistycznych-cnbop-pib-dane-za-2022-r?lang=pl")
 works = x.json()
 
-print('Title:')
-print(works['data']['attributes']['title'])
+typeIn = input('Type in what you want to know about this data. Commands are - title, time, desc, lang - ')
 
-print('Time of verification:')
-print(works['data']['attributes']['verified'])
-
-print('Description:')
-print(cleanhtml(works['data']['attributes']['description']))
-
-print('Language:')
-print(works['meta']['params']['lang'])
+if typeIn == 'title':
+  print('Title:')
+  print(works['data']['attributes']['title'])
+elif typeIn == 'time':
+  print('Time of verification:')
+  print(works['data']['attributes']['verified'])
+elif typeIn == 'desc':
+  print('Description:')
+  print(cleanhtml(works['data']['attributes']['description']))
+elif typeIn == 'lang':
+  print('Language:')
+  print(works['meta']['params']['lang'])
+else:
+  print('Unknown command.')
