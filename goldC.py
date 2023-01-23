574fa2936b75cb2442c7bfaf4873d626f1c1097c
Lonerlee <sokalsebastian96@gmail.com>
2023-01-23 20:43:55 +0100

    Adding prints

--- goldC.py
+++ goldC.py
@@ -4,12 +4,16 @@ import os
 token = os.environ['GOLD_TOKEN']
 
 headers = {
-    'x-access-token': token,
+  'x-access-token': token,
 }
 
 response = requests.get('https://www.goldapi.io/api/XAU/USD', headers=headers)
 
-if response.status_code == 200:
-    print('ok')
-else:
-    print(response.status_code)
+if response.status_code != 200:
+  print(response.status_code)
+  sys.exit()
+
+print('Status - working')
+getDataJson = response.json()
+print('The price of ' + getDataJson['metal'] + ' is: ' + str(getDataJson['price']) + ' ' + getDataJson['currency'])
+print('Name of the exchange is ' + getDataJson['exchange'])
