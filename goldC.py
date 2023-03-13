2dc10960ec2a8bc5fde47993168e99485fab15e5
Lonerlee <42679050+Lonerlee@users.noreply.github.com>
2023-03-13 11:56:11 +0100

    putting everything in function

--- goldC.py
+++ goldC.py
@@ -1,19 +1,23 @@
 import requests
 import os
 
-token = os.environ['GOLD_TOKEN']
+def getGoldAPI():
+  token = os.environ['GOLD_TOKEN']
 
-headers = {
-  'x-access-token': token,
-}
+  headers = {
+    'x-access-token': token,
+  }
 
-response = requests.get('https://www.goldapi.io/api/XAU/USD', headers=headers)
+  response = requests.get('https://www.goldapi.io/api/XAU/USD', headers=headers)
 
-if response.status_code != 200:
-  print(response.status_code)
-  sys.exit()
+  if response.status_code != 200:
+    print(response.status_code)
+    print('Status - ERROR')
+    sys.exit()
 
-print('Status - working')
-getDataJson = response.json()
-print('The price of ' + getDataJson['metal'] + ' is: ' + str(getDataJson['price']) + ' ' + getDataJson['currency'])
-print('Name of the exchange is ' + getDataJson['exchange'])
+  print('Status - working')
+  getDataJson = response.json()
+  print('The price of ' + getDataJson['metal'] + ' is: ' + str(getDataJson['price']) + ' ' + getDataJson['currency'])
+  print('Name of the exchange is ' + getDataJson['exchange'])
+  
+getGoldAPI()
